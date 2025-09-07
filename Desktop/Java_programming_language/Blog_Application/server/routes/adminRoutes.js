b02c0da8062594c4fe38f6169546a6f7fd56d56a
SujalPapalkar <sujalpapalkar15@gmail.com>
2025-09-07 18:26:54 +0530

    initial commit

--- Desktop/Java_programming_language/Blog_Application/server/routes/adminRoutes.js
+++ Desktop/Java_programming_language/Blog_Application/server/routes/adminRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { adminLogin, approveCommentById, deleteCommentById, getAllBlogsAdmin, getAllComments, getDashboard } from "../controllers/admincontroller.js";
+import { adminLogin, approveCommentById, deleteCommentById, getAllBlogsAdmin, getAllComments, getDashboard } from "../controllers/adminController.js";
 import auth from "../middleware/auth.js";
 
 const adminRouter=express.Router();
